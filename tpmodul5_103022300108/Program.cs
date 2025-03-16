fb29d31531cfd671b42b14b38f4710883ce64e6e
sidqi <sidqiarasyid31@gmail.com>
2025-03-16 21:24:52 +0700

    “menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul5_103022300108/Program.cs
+++ tpmodul5_103022300108/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class HaloGeneric
+{
+    public static void Main()
+    {
+        SapaUser("Sidqi Athallah Ar");
+    }
+    public static void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo User" + user);
+    }
+}
